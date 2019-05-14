78dfdc089912811d8dbf1f57f11a50d4b0fe6067
Sung W. J. Park <parksw723@gmail.com>
2019-05-14 18:41:13 +0800

    First commit

---
+++ readme.md
@@ -0,0 +1,39 @@
+Open source go package to enhance gorilla/mux
+
+##Features
+- Middleware
+- Convenience .get, .post, ...
+- Sub-routing, sub-middleware declarations
+
+##Demo Usage
+
+```
+r := syrup.New(mux.NewRouter())
+
+// Authentication
+func(api syrup.Router) {
+	api.Post("/password", login)
+	api.Post("/register", signup)
+}(r.Group("/auth"))
+
+// Limited to requests with sessions
+r.Use(secureMiddleware)
+
+// Fetch user
+r.Get("/user", getUser)
+// Update user details
+r.Post("/user", updateUser)
+// Update user password
+r.Post("/user/password", updateUserPassword)
+
+func secureMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if true {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		w.WriteHeader(http.StatusForbidden)
+	})
+}
+```
---
+++ utility.go
@@ -0,0 +1,50 @@
+package syrup
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type H map[string]interface{}
+
+func New(r *mux.Router, middleware ...http.HandlerFunc) Router {
+	return Router{r}
+}
+
+func Bind(w http.ResponseWriter, r *http.Request, out interface{}) error {
+	err := json.NewDecoder(r.Body).Decode(out)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	return err
+}
+
+func WriteJSON(w http.ResponseWriter, code int, out interface{}) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(code)
+
+	if err := json.NewEncoder(w).Encode(out); err != nil {
+		panic(err)
+	}
+}
+
+func WriteJSONChecksum(w http.ResponseWriter, code int, out interface{}) {
+	b := new(bytes.Buffer)
+	if err := json.NewEncoder(b).Encode(out); err != nil {
+		panic(err)
+	}
+
+	// calculate checksum and set as header
+	sum := sha256.Sum256(b.Bytes())
+	w.Header().Set("x-checksum", fmt.Sprintf("%x", sum))
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(code)
+
+	b.WriteTo(w)
+}
